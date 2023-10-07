3219a1cce168192ba97f24a3324d21a1dbc9a3b2
jumper <ariffnaj@gmail.com>
2023-10-07 09:41:02 +0800

    success login and pouchdb

--- main.js
+++ main.js
@@ -1,51 +1,96 @@
-const { app, BrowserWindow,ipcMain  } = require('electron')
-const path = require('node:path')
-const PouchDB = require('pouchdb');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const path = require('path')
 const axios = require('axios');
-const db = new PouchDB('mydb');
-// const bootstrap = require('bootstrap');
-const createWindow = () => {
-    // Create the browser window.
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true,
-        preload: path.join(__dirname, 'preload.js'),
-      }
-    })
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-      // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-  db.put({
-    _id: 'mydoc',
-    title: 'My Document',
-    content: 'Hello, PouchDB!',
-  });
-  }
-  // This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-  app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+const apiUrl = 'http://test-demo.aemenersol.com/api/account/login';
+const PouchDB = require('pouchdb');
+const db = new PouchDB('AuthDb');
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title)
+  })
+
+  mainWindow.loadFile('index.html')
+}
+function handleSetTitle(event, title) {
+  console.log('func from main.js');
+  const webContents = event.sender
+  const win = BrowserWindow.fromWebContents(webContents)
+  win.setTitle(title)
+}
+function handleMessage(event, title){
+  console.log('func from main.js', title);
+
+}
+async function handleFileOpen() {
+  console.log('func from main.js');
+
+  // const { canceled, filePaths } = await dialog.showOpenDialog()
+  // if (!canceled) {
+  //   return filePaths[0]
+  // }
+}
+app.whenReady().then( () => {
+
+  ipcMain.on("user:login", (event, data) => {
+
+    console.log('login main.js', JSON.parse(data));
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: apiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Cookie': '.AspNetCore.Identity.Application=CfDJ8Kci65wTckpIno-__NGTkT5rTEiFienE_qyQmGSpWrOeYw04AE4CyzsioIlPXTHnxot0NHQ-hmC1NbGiWoAc5QIHOQcEiONSN91pGnmY5BdjHImf-Slk6BrfA2L3TnXPRhyLZHYvKOeEj9-O7Y2FG7tYdaeD4krXx0zPItkKqzSOqG3NAwayieQqQ2z8QVvZd9B-3-8ElZSfOnQFcpEjRyJLcy1-bS-MgUhcNpyk93_-1bpzIYSzxhQGZFzHCXRinvajV7EKU_78WGdLiSEEDVNUPG4Fr4u1V1i_hintAXbRVOvHAc_BhfWnIRwXk6bpAe3BFl5vfQWPn9SxnvmmFcprsHDxuGER1ZZfv3aPWUHqTKCv60VBcOXmjuxwlHxOjK-L7LBzB2doFzupNlWltYPaJT_tt6wzGOdGMyPO6hVKpp1Q98X8stABkCGxCKakehloSFz89aqurARnBKoHXjXPLp5bE-qRYjpAxJyCD67Al2exf5T1yAiAuIDfjoHl-TuaAc2U5iNjWjjIiT14WVMq9TFGLCaxAPpxp0swbru_hKu0KbIbmXod4NqvujEuBLM--v1lphafCmTlOVpnVheihAjwJyC1jGrQIUcizZEJPR6C5w7fMDjFRy5qtiryHl-g9iBL13PQU0DWemwiEr_th1eb9lotpnNRTQ8IcKDWhKdHifK7oUaxgh71QzMs3w'
+      },
+      data: data
+    };
+     axios.request(config)
+      .then(async(response) => {
+        console.log(JSON.stringify(response.data));
+        storeData(JSON.parse(data), response.data);
       })
-
+      .catch((error) => {
+        console.log(error);
+      });
   })
-  // Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+  showTodos()
 
-//   axios.get('http://test-demo.aemenersol.com/api/dashboard').then(resp => {
+})
 
-//     console.log(resp.data);
-// });
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+ function storeData(data,token){
+   const document = {
+     _id: new Date().toISOString(),  // Provide a unique ID
+     username: data.username,
+     password: data.password,
+     token: token,
+   };
+   db.put(document)
+    .then((response) => {
+      console.log('Document saved:', response);
+    })
+    .catch((error) => {
+      console.error('Error saving document:', error);
+    });
+}
+function showTodos() {
+  db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
+    console.log(doc.rows);
+    // redrawTodosUI(doc.rows);
+  });
+}
--- renderer.js
+++ renderer.js
@@ -1,99 +1,14 @@
-// frontend js
+const loginbtn = document.getElementById('login')
+const Username = document.getElementById('Username')
+const Password = document.getElementById('password')
 
-// const { ipcRenderer } = require('electron');
-
-// const axios = require('axios');
-
-// const dataForm = document.getElementById('dataForm');
-// const apiResponseDiv = document.getElementById('output');
-
-const Username = document.getElementById('Username');
-const Password = document.getElementById('password');
-
-// dataForm.addEventListener('click', () => {
-//     axios.post('http://test-demo.aemenersol.com/api/account/login',
-//     {
-//         username: Username,
-//         password: Password
-//       })
-//       .then((response) => {
-//         const responseData = response.data;
-//         apiResponseDiv.textContent = `API Response: ${JSON.stringify(responseData)}`;
-//       })
-//       .catch((error) => {
-//         apiResponseDiv.textContent = `API Error: ${error.message}`;
-//       });
-//   });
-const electron = require('electron');
-// Importing the net Module from electron remote
-const net = electron.remote.net;
-const dataForm = document.getElementById('dataForm');
-// let get = document.getElementById('get');
-// dataForm.addEventListener('click', () => {
-// 	const request = net.request({
-// 		method: 'GET',
-// 		protocol: 'http:',
-// 		hostname: 'httpbin.org',
-// 		path: '/get',
-// 		redirect: 'follow'
-// 	});
-// 	request.on('response', (response) => {
-// 		console.log(`STATUS: ${response.statusCode}`);
-// 		console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-
-// 		response.on('data', (chunk) => {
-// 			console.log(`BODY: ${chunk}`)
-// 		});
-// 	});
-// 	request.on('finish', () => {
-// 		console.log('Request is Finished')
-// 	});
-// 	request.on('abort', () => {
-// 		console.log('Request is Aborted')
-// 	});
-// 	request.on('error', (error) => {
-// 		console.log(`ERROR: ${JSON.stringify(error)}`)
-// 	});
-// 	request.on('close', (error) => {
-// 		console.log('Last Transaction has occurred')
-// 	});
-// 	request.setHeader('Content-Type', 'application/json');
-// 	request.end();
-// });
-
-// let post = document.getElementById('post');
-dataForm.addEventListener('click', () => {
-	let body = JSON.stringify({   
-        username: Username,
-        password: Password });
-	const request = net.request({
-		method: 'POST',
-		protocol: 'http:',
-		hostname: 'test-demo.aemenersol.com/api/account/login',
-		path: '/post',
-		redirect: 'follow'
-	});
-	request.on('response', (response) => {
-		console.log(`STATUS: ${response.statusCode}`);
-		console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-
-		response.on('data', (chunk) => {
-			console.log(`BODY: ${chunk}`)
-		});
-	});
-	request.on('finish', () => {
-		console.log('Request is Finished')
-	});
-	request.on('abort', () => {
-		console.log('Request is Aborted')
-	});
-	request.on('error', (error) => {
-		console.log(`ERROR: ${JSON.stringify(error)}`)
-	});
-	request.on('close', (error) => {
-		console.log('Last Transaction has occurred')
-	});
-	request.setHeader('Content-Type', 'application/json');
-	request.write(body, 'utf-8');
-	request.end();
-});
+loginbtn.addEventListener('click', async () => {
+	console.log('render', Username.va);
+	// const filePath = await window.electronAPI.openFile()
+	// filePathElement.innerText = filePath
+	const data = {
+		username: Username.value,
+		password: Password.value,
+	};
+	window.electron.login(JSON.stringify(data));
+})
